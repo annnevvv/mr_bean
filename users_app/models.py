3f995fdb769488edf4c65e336d25649997d82f5d
Anna Walaszek <noreplay@annn.evvv>
2023-11-19 20:23:20 +0100

    added tiers, user profile and signals to models.py

--- users_app/models.py
+++ users_app/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+
+class UserAccountTier(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    mini_size = models.ManyToManyField(MiniatureSize, null=True)
+    presence_link_to_org_file = models.BooleanField(default=0)
+    ability_to_generate_expiring_links = models.BooleanField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+def user_avatar_image_path(instance, filename):
+    return f'users/{instance.user.id}/avatar/{filename}'
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    tier = models.ForeignKey(UserAccountTier,
+                             on_delete=models.CASCADE,
+                             default=1)
+    avatar = models.ImageField(upload_to=user_avatar_image_path,
+                               default='koteł.jpg')
+    points = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.user.username}"
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
