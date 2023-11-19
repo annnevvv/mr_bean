c59597d2781a3ba9c9e0a0de2c95c0695c9c1d4a
Anna Walaszek <noreplay@annn.evvv>
2023-11-19 17:33:43 +0100

    start project

--- mr_bean/settings.py
+++ mr_bean/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from z_utils.passwords import SEC_KEY
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k@qxu+f%2e$wx7wpvul*#7etbhhy^9t1)0)q3(k&#*@5gs^he)'
+SECRET_KEY = SEC_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
