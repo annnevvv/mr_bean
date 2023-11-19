15bd639dcc63efcbb6dbd40e169c53de028b60c6
Anna Walaszek <noreplay@annn.evvv>
2023-11-19 20:03:31 +0100

    added debug settings to urls.py

--- mr_bean/urls.py
+++ mr_bean/urls.py
@@ -18,9 +18,11 @@ from rest_framework import routers
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
-router = routers.DefaultRouter()
+# router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,3 +31,7 @@ urlpatterns = [
     path('api/', include('image_app.urls', 'api_app'))
     # path('api/', include(router.urls), name='api'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT) + static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT)
