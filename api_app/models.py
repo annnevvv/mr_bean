569fd9f9b30c7f8396cce50533bcacc85d822265
Anna Walaszek <noreplay@annn.evvv>
2023-11-22 21:55:33 +0100

    added admin set and imports to model.py to users_a

--- api_app/models.py
+++ api_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MiniatureSize(models.Model):
+    height = models.PositiveIntegerField()
+    width = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.height}x{self.width}'
